93c92ad24c44a9966d4a12c5e9c94d4d012ade41
Isah_F <ksiohmf1@gmail.com>
2026-03-03 23:27:26 -0300

    03-03

---
+++ Aula - Compilado JS/Aula - Mais JS/script.js
@@ -0,0 +1,44 @@
+console.log('Teste de Script interno');
+
+// Tipos de váriaveis:
+
+//let:
+let teste1 = 'maçã';
+
+//const:
+const teste2 = 'Laranja';
+console.log(teste2);
+
+//var:
+var teste3 = 'pêra';
+
+if (teste3){
+    var teste123 = 'uva';
+}
+console.log(teste123);
+
+//string:
+let nome = 'maçã';
+
+//numero e concatenar:
+let numero = 16;
+
+console.log('Numero é' + numero * 2 + '!');
+console.log('Meu nome é:'+ nome);
+
+console.log("6" + 4);
+console.log("6" - 4);
+console.log("6" * 4);
+console.log("6" / 4);
+
+//booleanos
+let maiorDeIdade = numero >= 18;
+console.log(maiorDeIdade);
+
+//null e undefind
+let testenull = null;
+let testeundefind;
+
+console.log(testenull);
+console.log(testeundefind *2);
+
--- Aula - Compilado JS/script.js
+++ Aula - Compilado JS/script.js
@@ -1,29 +1,96 @@
 // Tipos de váriaveis:
 
-//let:
-let teste1 = 'Isah';
+//let (permite alterar o valor):
+let teste1 = 'maçã';
 
-//const:
-const teste2 = 'Isah2';
+//const (não permite novo valor):
+const teste2 = 'Laranja';
 console.log(teste2);
 
 //var:
-var teste3 = 'Isah3';
+var teste3 = 'Pêra';
 
-if (teste3){
-    var teste123 = 'Isah4';
+if (teste3){ //Atribui 'Isah4' à variável teste123
+    var teste123 = 'Uva';
 }
 console.log(teste123);
 
 //string:
 let nome = 'Isah';
 
-//numero:
+//número:
 let numero = 16;
 
-console.log('Numero é' + numero * 2 + '!');
-console.log('Meu nome é:'+ nome);
+console.log('Numero é ' + numero * 2 + '!');
+console.log('Meu nome é: '+ nome);
 
-//booleanos
+//booleanos:
 let maiorDeIdade = numero >= 18;
 console.log(maiorDeIdade);
+
+//null e undefind
+let testenull = null;
+let testeundefind;
+
+console.log(testenull);
+console.log(testeundefind *2);
+
+
+/////////////////////////////////////////////
+
+//concatenar:
+console.log("6" + 4);
+console.log("6" - 4);
+console.log("6" * 4);
+console.log("6" / 4);
+
+
+//Operadores:
+
+let contador = 0;
+contador ++;    //contador +1
+contador +=3;   //contador +3
+contador --;    //contador -1
+contador -= 5;  //contador -5
+
+console.log(contador);  //contador=(-2)
+
+////////////////////////////////////////////
+
+//Condição:
+let numero2 =3;
+let testeNull = null;
+
+if (testeNull != null){
+    console.log("Não é nulo.");
+}
+else if (numero2 >4){
+    console.log("Número maior do que 4!");
+}
+else{
+    console.log("É nulo.");
+}
+
+
+if ("85" == 85){  //converte os valores
+    console.log("Verdadeiro!")
+}
+if ("85" === 85){ //compara valor e tipo
+    console.log("Falso!")
+}
+
+//////////////////////////////////////
+
+//Conversão de tipos:
+let num = 1;
+let numString = String(num); //convertendo o número 1 para string.
+let string = "86";
+let stringNum = Number(string); //convertendo o número 86 para número.
+
+console.log(typeof num,num);
+console.log(typeof numString,numString);
+console.log(typeof string,string);
+console.log(typeof stringNum,stringNum);
+
+///////////////////////////////////
+
