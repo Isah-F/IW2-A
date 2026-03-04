57768faeadf610b8799efd878961025d6cfc2653
Isah_F <ksiohmf1@gmail.com>
2026-03-03 23:33:07 -0300

    03-03

---
+++ Exercícios - JS/script.js
@@ -0,0 +1,29 @@
+//1. Variáveis e Exibição:
+let nome = 'Isabela';
+let idade = '16';
+let curso = 'Infonet';
+console.log("Olá eu sou a "+ nome,"tenho " + idade, "anos e faço parte do curso " + curso, "segundo período.");
+
+//2. Soma de Dois Números:
+let num1 = 20;
+let num2 = 10;
+let soma = num1 + num2;
+console.log("Soma = " + soma);
+
+//3. Operações Matemáticas
+console.log("Soma = " + soma);
+
+let sub = num1 - num2;
+console.log("Subtração = " + sub);
+
+let mult = num1 * num2;
+console.log("Multiplicação = " + mult);
+
+let div = num1 / num2;
+console.log("Divisão = " + div);
+
+let resto = num1%num2; //refazer
+console.log("Resto da divisão = " + div);
+
+//4. Conversão de String para Número:
+let numero = "30";
