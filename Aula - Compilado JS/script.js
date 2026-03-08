700bfa0e35098918a50226942c5e139536f582eb
Isah_F <ksiohmf1@gmail.com>
2026-03-07 23:54:46 -0300

    07-03

--- Aula - Compilado JS/script.js
+++ Aula - Compilado JS/script.js
@@ -1,13 +1,16 @@
 // Tipos de váriaveis:
 
 //let (permite alterar o valor):
-let teste1 = 'maçã';
+let teste = 2;
+console.log(teste);
+teste = 4;
+console.log(teste);
 
 //const (não permite novo valor):
 const teste2 = 'Laranja';
 console.log(teste2);
 
-//var:
+//var - variáveis que podem ter o seu valor alterado, porém, com escopo global
 var teste3 = 'Pêra';
 
 if (teste3){ //Atribui 'Isah4' à variável teste123
@@ -38,48 +41,11 @@ console.log(testeundefind *2);
 
 /////////////////////////////////////////////
 
-//concatenar:
-console.log("6" + 4);
-console.log("6" - 4);
-console.log("6" * 4);
-console.log("6" / 4);
-
-
-//Operadores:
-
-let contador = 0;
-contador ++;    //contador +1
-contador +=3;   //contador +3
-contador --;    //contador -1
-contador -= 5;  //contador -5
-
-console.log(contador);  //contador=(-2)
-
-////////////////////////////////////////////
-
-//Condição:
-let numero2 =3;
-let testeNull = null;
-
-if (testeNull != null){
-    console.log("Não é nulo.");
-}
-else if (numero2 >4){
-    console.log("Número maior do que 4!");
-}
-else{
-    console.log("É nulo.");
-}
-
-
-if ("85" == 85){  //converte os valores
-    console.log("Verdadeiro!")
-}
-if ("85" === 85){ //compara valor e tipo
-    console.log("Falso!")
-}
-
-//////////////////////////////////////
+//concatenar - conversão:
+console.log("6" + 4); //string
+console.log("6" - 4); //number
+console.log("6" * 4); //number
+console.log("6" / 4); //number
 
 //Conversão de tipos:
 let num = 1;
@@ -93,4 +59,96 @@ console.log(typeof string,string);
 console.log(typeof stringNum,stringNum);
 
 ///////////////////////////////////
+
+//Operadores:
+
+let contador = 0;
+contador ++;    //contador +1
+contador +=3;   //contador +3
+contador --;    //contador -1
+contador -= 5;  //contador -5
+
+console.log(contador);  //contador=(-2)
+
+////////////////////////////////////////////
+
+//>= / <= / || / && / == / === (além de valor, compara o tipo)...
+
+if (idade >= 18 && testeNull == null) //Sem chaves para if's de uma linha1
+    console.log('Teste');
+
+if ("85" == 85){  //converte os valores
+    console.log("Verdadeiro!")
+}
+if ("85" === 85){ //compara valor e tipo
+    console.log("Falso!")
+}
+////////////////////////////////////////////
+
+//Condição:
+if (testeNull != null) {
+    console.log('Não é nulo');
+}
+else {
+    console.log('Nulo');
+}
+
+
+if (idade) {
+    console.log('Idade existe e foi definida!');
+}
+else {
+    console.log('Idade não definida');
+}
+
+
+if (testeUndefined) {
+    console.log('Undefined tem valor!');
+}
+else if (testeNull) {
+    console.log('Null tem valor!');
+}
+else {
+    console.log('Caímos no else!');
+}
+
+//////////////////////////////////////
+
+//Repetição:
+//for 
+
+for (let i = 0; i <= 10; i++) {
+    console.log('Printando: ' + i);
+}
+
+//while
+let cont = 3;
+while (cont >= 0) {
+    console.log('contador: ' + cont);
+    cont--;
+}
+
+//////////////////////////////////////
+
+//Funções:
+
+//Se a minha função tiver 'return', ela retorna algo. Se não, é 'void'
+function verificaSeParOuImpar(num) {
+    if (num % 2 == 0)
+        console.log('É par!');
+    else
+        console.log('É impar!');
+}
+
+verificaSeParOuImpar(3);
+verificaSeParOuImpar(6);
+
+function multiplicaPorDois(valor) {
+    return valor * 2;
+}
+
+let teste4 = multiplicaPorDois(2);
+console.log(teste4);
+
+//////////////////////////////////////
 
--- Exercícios - JS/script.js
+++ Exercícios - JS/script.js
@@ -1,17 +1,19 @@
+//Isabela Oliveira Fernandes    2ºInfonet 
+
 //1. Variáveis e Exibição:
 let nome = 'Isabela';
-let idade = '16';
+let idade1 = '16';
 let curso = 'Infonet';
-console.log("Olá eu sou a "+ nome,"tenho " + idade, "anos e faço parte do curso " + curso, "segundo período.");
+console.log("Olá eu sou a "+ nome,"tenho " + idade1, "anos e faço parte do curso " + curso, "segundo período.");
 
 //2. Soma de Dois Números:
 let num1 = 20;
 let num2 = 10;
-let soma = num1 + num2;
-console.log("Soma = " + soma);
+let soma1 = num1 + num2;
+console.log("Soma = " + soma1);
 
 //3. Operações Matemáticas
-console.log("Soma = " + soma);
+console.log("Soma = " + soma1);
 
 let sub = num1 - num2;
 console.log("Subtração = " + sub);
@@ -23,7 +25,91 @@ let div = num1 / num2;
 console.log("Divisão = " + div);
 
 let resto = num1%num2; //refazer
-console.log("Resto da divisão = " + div);
+console.log("Resto da divisão = " + resto);
 
 //4. Conversão de String para Número:
-let numero = "30";
+let string = "30";
+let stringNum = Number(string); 
+console.log("Soma:", stringNum + 20);
+
+//5. Comparações:
+let a = 10;
+let b ="10";
+
+let test1 = (a == b);
+console.log(test1);
+
+let test2 = (a === b);
+console.log(test2);
+
+//6. Verificação de Maioridade:
+let idade2 = 16;
+let maiorDeIdade = 18;
+
+if (idade2 >= maiorDeIdade){
+    console.log("Maior de idade.")
+}
+else{
+    console.log("Menor de idade.")
+}
+
+//7.Verificação de Número Positivo ou Negativo:
+let number = -2;
+if (number > 0){
+    console.log("O número " + number, "é positivo.");
+}
+else if(number <0){
+    console.log("O número " +number, "é negativo.");
+}
+else{
+    console.log("O número " +number, "é zero.");
+}
+
+//8.Classificação de Nota:
+let nota = 8;
+if (nota >= 7){
+    console.log("Aprovado.");
+}
+else if(nota <5){
+    console.log("Reprovado");
+}
+else{
+    console.log("Recuperação");
+}
+
+//9. Laço For:
+for (let i = 1; i <= 10; i++) {
+    console.log(+ i);
+}
+
+//10. Soma de 1 a 100:
+let soma2 = 0;
+for (let i = 1; i <= 100; i++) {
+    soma2 += i;
+}
+console.log("Resultado: " + soma2);
+
+//11. While:
+let cont = 5;
+while (cont >= 1) {
+    console.log(cont);
+    cont--;
+}
+
+//12. Função Simples:
+function DoisNum (c, d) {
+    return c + d;
+}
+let resultado = DoisNum (2, 2);
+console.log("Resultado: " + resultado);
+
+//13. Função Par ou Ímpar:
+function ParOuImpar(num3) {
+    if (num3 % 2 == 0)
+        console.log(num3 + " é par.");
+    else
+        console.log(num3 +" é impar.");
+}
+
+ParOuImpar(2);
+ParOuImpar(7);
